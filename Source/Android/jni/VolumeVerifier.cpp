9fc20368e3ea283c87802d5e3e9e4d57345f6c8e
JosJuice <josjuice@gmail.com>
2023-02-18 13:04:40 +0100

    fixup Volume allocation

--- Source/Android/jni/VolumeVerifier.cpp
+++ Source/Android/jni/VolumeVerifier.cpp
@@ -1,15 +1,25 @@
 // Copyright 2022 Dolphin Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include <memory>
+#include <utility>
+
 #include <jni.h>
 
+#include "DiscIO/Volume.h"
 #include "DiscIO/VolumeVerifier.h"
 #include "jni/AndroidCommon/AndroidCommon.h"
 #include "jni/AndroidCommon/IDCache.h"
 
-static DiscIO::VolumeVerifier* GetPointer(JNIEnv* env, jobject obj)
+struct VolumeVerifierContext
 {
-  return reinterpret_cast<DiscIO::VolumeVerifier*>(
+  DiscIO::VolumeVerifier verifier;
+  std::unique_ptr<DiscIO::Volume> volume;
+};
+
+static VolumeVerifierContext* GetContext(JNIEnv* env, jobject obj)
+{
+  return reinterpret_cast<VolumeVerifierContext*>(
       env->GetLongField(obj, IDCache::GetVolumeVerifierPointer()));
 }
 
@@ -18,7 +28,7 @@ extern "C" {
 JNIEXPORT void JNICALL Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_finalize(
     JNIEnv* env, jobject obj)
 {
-  delete GetPointer(env, obj);
+  delete GetContext(env, obj);
 }
 
 JNIEXPORT jlong JNICALL
@@ -26,109 +36,113 @@ Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_createNew(
     JNIEnv* env, jobject obj, jstring path, jboolean redump_verification, jboolean calculate_crc32,
     jboolean calculate_md5, jboolean calculate_sha1)
 {
-  const std::unique_ptr<DiscIO::Volume> volume = DiscIO::CreateVolume(GetJString(env, path));
+  std::unique_ptr<DiscIO::Volume> volume = DiscIO::CreateVolume(GetJString(env, path));
   if (!volume)
     return 0;
 
-  auto* verifier = new DiscIO::VolumeVerifier(
-      *volume, redump_verification, {!!calculate_crc32, !!calculate_md5, !!calculate_sha1});
+  auto* context = new VolumeVerifierContext{
+      DiscIO::VolumeVerifier{
+          *volume, !!redump_verification, {!!calculate_crc32, !!calculate_md5, !!calculate_sha1}},
+      nullptr};
 
-  return reinterpret_cast<jlong>(verifier);
+  context->volume = std::move(volume);
+
+  return reinterpret_cast<jlong>(context);
 }
 
 JNIEXPORT void JNICALL
 Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_start(JNIEnv* env, jobject obj)
 {
-  GetPointer(env, obj)->Start();
+  GetContext(env, obj)->verifier.Start();
 }
 
 JNIEXPORT void JNICALL Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_process(
     JNIEnv* env, jobject obj)
 {
-  GetPointer(env, obj)->Process();
+  GetContext(env, obj)->verifier.Process();
 }
 
 JNIEXPORT jlong JNICALL
 Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_getBytesProcessed(JNIEnv* env,
                                                                                       jobject obj)
 {
-  return static_cast<jlong>(GetPointer(env, obj)->GetBytesProcessed());
+  return static_cast<jlong>(GetContext(env, obj)->verifier.GetBytesProcessed());
 }
 
 JNIEXPORT jlong JNICALL
 Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_getTotalBytes(JNIEnv* env,
                                                                                   jobject obj)
 {
-  return static_cast<jlong>(GetPointer(env, obj)->GetTotalBytes());
+  return static_cast<jlong>(GetContext(env, obj)->verifier.GetTotalBytes());
 }
 
 JNIEXPORT void JNICALL
 Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_finish(JNIEnv* env, jobject obj)
 {
-  GetPointer(env, obj)->Process();
+  GetContext(env, obj)->verifier.Process();
 }
 
 JNIEXPORT jstring JNICALL
 Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_getResultSummaryText(
     JNIEnv* env, jobject obj)
 {
-  return ToJString(env, GetPointer(env, obj)->GetResult().summary_text);
+  return ToJString(env, GetContext(env, obj)->verifier.GetResult().summary_text);
 }
 
 JNIEXPORT jint JNICALL
 Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_getResultRedumpStatus(
     JNIEnv* env, jobject obj)
 {
-  return static_cast<jint>(GetPointer(env, obj)->GetResult().redump.status);
+  return static_cast<jint>(GetContext(env, obj)->verifier.GetResult().redump.status);
 }
 
 JNIEXPORT jstring JNICALL
 Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_getResultRedumpMessage(
     JNIEnv* env, jobject obj)
 {
-  return ToJString(env, GetPointer(env, obj)->GetResult().redump.message);
+  return ToJString(env, GetContext(env, obj)->verifier.GetResult().redump.message);
 }
 
 JNIEXPORT jbyteArray JNICALL
 Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_getResultCrc32(JNIEnv* env,
                                                                                    jobject obj)
 {
-  return VectorToJByteArray(env, GetPointer(env, obj)->GetResult().hashes.crc32);
+  return VectorToJByteArray(env, GetContext(env, obj)->verifier.GetResult().hashes.crc32);
 }
 
 JNIEXPORT jbyteArray JNICALL
 Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_getResultMd5(JNIEnv* env,
                                                                                  jobject obj)
 {
-  return VectorToJByteArray(env, GetPointer(env, obj)->GetResult().hashes.md5);
+  return VectorToJByteArray(env, GetContext(env, obj)->verifier.GetResult().hashes.md5);
 }
 
 JNIEXPORT jbyteArray JNICALL
 Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_getResultSha1(JNIEnv* env,
                                                                                   jobject obj)
 {
-  return VectorToJByteArray(env, GetPointer(env, obj)->GetResult().hashes.sha1);
+  return VectorToJByteArray(env, GetContext(env, obj)->verifier.GetResult().hashes.sha1);
 }
 
 JNIEXPORT jint JNICALL
 Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_getResultProblemCount(
     JNIEnv* env, jobject obj)
 {
-  return static_cast<jint>(GetPointer(env, obj)->GetResult().problems.size());
+  return static_cast<jint>(GetContext(env, obj)->verifier.GetResult().problems.size());
 }
 
 JNIEXPORT jint JNICALL
 Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_getResultProblemSeverity(
     JNIEnv* env, jobject obj, jint i)
 {
-  return static_cast<jint>(GetPointer(env, obj)->GetResult().problems[i].severity);
+  return static_cast<jint>(GetContext(env, obj)->verifier.GetResult().problems[i].severity);
 }
 
 JNIEXPORT jstring JNICALL
 Java_org_dolphinemu_dolphinemu_features_verify_model_VolumeVerifier_getResultProblemText(
     JNIEnv* env, jobject obj, jint i)
 {
-  return ToJString(env, GetPointer(env, obj)->GetResult().problems[i].text);
+  return ToJString(env, GetContext(env, obj)->verifier.GetResult().problems[i].text);
 }
 
 JNIEXPORT jboolean JNICALL
