50d90a14089dfc2c90b6b19c947354306ed8283f
JosJuice <josjuice@gmail.com>
2023-12-03 12:37:16 +0100

    JitArm64: Actually set Arm64RegCache::m_reg_stats

--- Source/Core/Core/PowerPC/JitArm64/JitArm64_RegCache.cpp
+++ Source/Core/Core/PowerPC/JitArm64/JitArm64_RegCache.cpp
@@ -147,8 +147,9 @@ Arm64GPRCache::Arm64GPRCache() : Arm64RegCache(GUEST_GPR_COUNT + GUEST_CR_COUNT)
 {
 }
 
-void Arm64GPRCache::Start(PPCAnalyst::BlockRegStats& stats)
+void Arm64GPRCache::Start(const PPCAnalyst::BlockRegStats& stats)
 {
+  m_reg_stats = &stats;
 }
 
 bool Arm64GPRCache::IsCallerSaved(ARM64Reg reg) const
@@ -484,6 +485,11 @@ constexpr size_t GUEST_FPR_COUNT = 32;
 
 Arm64FPRCache::Arm64FPRCache() : Arm64RegCache(GUEST_FPR_COUNT)
 {
+}
+
+void Arm64FPRCache::Start(const PPCAnalyst::BlockRegStats& stats)
+{
+  m_reg_stats = &stats;
 }
 
 void Arm64FPRCache::Flush(FlushMode mode, ARM64Reg tmp_reg)
